f5936c5a4b9138b621eac236882caa0664a95e1f
Sammuel Reis <sammugr@hotmail.com>
2021-06-12 14:38:27 -0300

    feat: implements theme button layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,82 @@
-import React from 'react';
-import { View, Text, StatusBar, StyleSheet } from 'react-native';
+import React from "react";
+import {
+  Image,
+  View,
+  Text,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+
+import lightModeIcon from "../assets/icons/light-mode.png";
+import darkModeIcon from "../assets/icons/dark-mode.png";
 
 export function Header() {
   return (
     <View style={styles.header}>
-      <Text style={styles.headerText}>to.</Text>
-      <Text style={[styles.headerText, { fontFamily: 'Poppins-SemiBold' }]}>do</Text>
+      <View style={styles.emptyContainer} />
+
+      <View style={styles.headerTitle}>
+        <Text style={styles.headerText}>to.</Text>
+        <Text style={[styles.headerText, { fontFamily: "Poppins-SemiBold" }]}>
+          do
+        </Text>
+      </View>
+
+      <View style={styles.headerRightContainer}>
+        <TouchableOpacity style={styles.headerRightButton} onPress={() => {}}>
+          <Image
+            resizeMode="cover"
+            source={darkModeIcon}
+            style={styles.themeModeIcon}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   header: {
     paddingTop: StatusBar.currentHeight,
     paddingBottom: 44,
-    backgroundColor: '#273FAD',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row'
+    backgroundColor: "#273FAD",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  emptyContainer: {
+    width: "33.3%",
+    height: 20,
+  },
+  headerTitle: {
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "33.3%",
   },
   headerText: {
     fontSize: 24,
-    color: '#FFF',
-    fontFamily: 'Poppins-Regular',
-  }
+    color: "#FFF",
+    fontFamily: "Poppins-Regular",
+  },
+  headerRightContainer: {
+    alignItems: "flex-end",
+    justifyContent: "center",
+    paddingRight: 16,
+    width: "33.3%",
+    height: 20,
+  },
+  headerRightButton: {
+    backgroundColor: "#34313D",
+    borderRadius: 5,
+    height: 32,
+    width: 32,
+    alignItems: "center",
+    justifyContent: "center",
+    elevation: 5,
+  },
+  themeModeIcon: {
+    width: 16,
+    height: 16,
+  },
 });
